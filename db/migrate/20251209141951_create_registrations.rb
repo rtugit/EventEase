f38573569ed40a4e81013fd3e693be02a616f6f1
Ahmed Nadir <28578413+TechnoVen@users.noreply.github.com>
2025-12-09 15:57:31 +0100

    Updated implemented Registration model

--- db/migrate/20251209141951_create_registrations.rb
+++ db/migrate/20251209141951_create_registrations.rb
@@ -2,6 +2,7 @@ class CreateRegistrations < ActiveRecord::Migration[7.1]
   def change
     create_table :registrations do |t|
       t.references :event, null: false, foreign_key: true
+      t.references :user, foreign_key: true, null: true
       t.string :email, null: false
       t.string :name
       t.string :status, null: false, default: "registered"
