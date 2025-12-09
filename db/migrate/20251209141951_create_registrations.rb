a04a93ee39f84d44df9df3485fefba664612e2ad
Ahmed Nadir <28578413+TechnoVen@users.noreply.github.com>
2025-12-09 15:24:57 +0100

    Registration Model Implemented

---
+++ db/migrate/20251209141951_create_registrations.rb
@@ -0,0 +1,15 @@
+class CreateRegistrations < ActiveRecord::Migration[7.1]
+  def change
+    create_table :registrations do |t|
+      t.references :event, null: false, foreign_key: true
+      t.string :email, null: false
+      t.string :name
+      t.string :status, null: false, default: "registered"
+      t.datetime :check_in_at
+      t.datetime :cancelled_at
+
+      t.timestamps
+    end
+    add_index :registrations, [:event_id, :email], unique: true
+  end
+end
