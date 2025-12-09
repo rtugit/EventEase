6fb9c594b4841db8345deb6a180c0bf231a351c5
Leonardo Van den Berghe <leonardovdb.lvdb@gmail.com>
2025-12-09 17:34:36 +0100

    made sure that the redirections of the log in page and sign up page are correct

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  
+  def after_sign_in_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_up_path_for(resource)
+    dashboard_path
+  end
+
+  def after_sign_out_path_for(resource)
+    "/users/sign_in"
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -33,6 +33,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_09_141951) do
 
   create_table "registrations", force: :cascade do |t|
     t.bigint "event_id", null: false
+    t.bigint "user_id"
     t.string "email", null: false
     t.string "name"
     t.string "status", default: "registered", null: false
@@ -42,6 +43,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_09_141951) do
     t.datetime "updated_at", null: false
     t.index ["event_id", "email"], name: "index_registrations_on_event_id_and_email", unique: true
     t.index ["event_id"], name: "index_registrations_on_event_id"
+    t.index ["user_id"], name: "index_registrations_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -61,4 +63,5 @@ ActiveRecord::Schema[7.1].define(version: 2025_12_09_141951) do
 
   add_foreign_key "events", "users", column: "organizer_id"
   add_foreign_key "registrations", "events"
+  add_foreign_key "registrations", "users"
 end
