47ad84868689f9532c441f36cad1568627a20aee
Richard Theodor Uehre <stuff@uehre.com>
2025-12-11 15:32:04 +0100

    Add new landing page layout and splash screen

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema[7.1].define(version: 2025_12_10_232752) do
-=======
-ActiveRecord::Schema[7.1].define(version: 2025_12_10_170743) do
->>>>>>> 47f22ac3f5836ff1fd2c065108515b07a49648b4
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
